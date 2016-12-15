da860c5cf2261a1ca61644336bc1633385555a4b
Manideepkota0708 <clmnsmanideep@gmail.com>
2016-12-15 15:23:00 +0530

    Finally

--- app/src/main/java/com/example/mani_4647/jsonarraytolist/MainActivity.java
+++ app/src/main/java/com/example/mani_4647/jsonarraytolist/MainActivity.java
@@ -2,78 +2,106 @@ package com.example.mani_4647.jsonarraytolist;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
-public class MainActivity extends AppCompatActivity implements LoadJSONTask.Listener, AdapterView.OnItemClickListener {
 
-    private ListView list_view;
+public class MainActivity extends AppCompatActivity {
+    String internetData;
+    StringBuffer stringBuffer;
 
-    public static final String URL = "http://www.json-generator.com/api/json/get/bYFgNayPyq?indent=2";
-
-    private List<HashMap<String, String>> people_hash_map_list = new ArrayList<>();
-
-    private static final String KEY_USER_ID = "userId";
-    private static final String KEY_ID = "id";
-    private static final String KEY_TEXT = "text";
-    private static final String KEY_BODY = "body";
+    OkHttpClient client = new OkHttpClient();
+    List<HashMap<String, String>> peopleList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        list_view = (ListView) findViewById(R.id.list_view);
-        list_view.setOnItemClickListener(this);
-        new LoadJSONTask(this).execute(URL);
-    }
-
-    @Override
-    public void onLoaded(List<People> people_list) {
-
-        for (People pep : people_list) {
-
-            HashMap<String, String> map = new HashMap<>();
-
-            map.put(KEY_BODY,pep.getBody());
-            map.put(KEY_USER_ID, pep.getUserId());
-            map.put(KEY_ID, pep.getId());
-            map.put(KEY_TEXT, pep.getTitle());
-
-            people_hash_map_list.add(map);
+        try {
+            run();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-
-        loadListView();
     }
 
-    @Override
-    public void onError() {
+    public void run() throws Exception {
+        Request request = new Request.Builder()
+                .url("http://jsonplaceholder.typicode.com/posts")
+                .build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
 
-        Toast.makeText(this, "Error !", Toast.LENGTH_SHORT).show();
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+
+                stringBuffer = new StringBuffer(response.body().string());
+
+                MainActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        // Toast.makeText(MainActivity.this, stringBuffer.toString(), Toast.LENGTH_SHORT).show();
+                        internetData = stringBuffer.toString();
+                        String stringFormOfJsonObject = null;
+                        try {
+                            JSONArray jsonArray = new JSONArray(internetData);
+                            JSONObject jsonObject;
+
+                            for (int i = 0; i < jsonArray.length(); i++) {
+                                jsonObject = jsonArray.getJSONObject(i);
+                                stringFormOfJsonObject = jsonObject.toString();
+
+                                String userId = jsonObject.getString("userId");
+                                String id = jsonObject.getString("id");
+                                String title = jsonObject.getString("title");
+                                String body = jsonObject.getString("body");
+
+                                HashMap<String, String> person = new HashMap<>();
+                                person.put("userId", userId);
+                                person.put("id", id);
+                                person.put("title", title);
+                                person.put("body", body);
+
+                                peopleList.add(person);
+
+                            }
+                            ListAdapter listAdapter;
+                            listAdapter = new SimpleAdapter(MainActivity.this, peopleList, R.layout.single_item_layout,
+                                    new String[]{"userId", "id", "title", "body"},
+                                    new int[]{R.id.etUserId, R.id.etId, R.id.etTitle, R.id.etBody});
+
+                            ListView listView = (ListView) findViewById(R.id.lvJSONData);
+                            listView.setAdapter(listAdapter);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        // Toast.makeText(MainActivity.this, stringFormOfJsonObject, Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+            }
+        });
     }
 
 
-    @Override
-    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-
-        Toast.makeText(this, people_hash_map_list.get(i).get(KEY_ID),Toast.LENGTH_LONG).show();
-    }
-
-    private void loadListView() {
-
-        ListAdapter adapter = new SimpleAdapter(MainActivity.this, people_hash_map_list, R.layout.list_item,
-                new String[] { KEY_BODY,KEY_USER_ID, KEY_ID, KEY_TEXT},
-                new int[] {R.id.text_view_body,R.id.text_view_user_id,R.id.text_view_id, R.id.text_view_text });
-
-        list_view.setAdapter(adapter);
-
-    }
 }
